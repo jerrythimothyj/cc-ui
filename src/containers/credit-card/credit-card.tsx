a18b62ef58227f67e2a0e3db71115da464cbd113
Jerry <jerrythimothy@gmail.com>
2019-10-16 21:20:01 +0530

    Added form

--- src/containers/credit-card/credit-card.tsx
+++ src/containers/credit-card/credit-card.tsx
@@ -1,30 +1,14 @@
 import React from "react";
+import AddCreditCard from "../../components/add-credit-card/add-credit-card";
+import CreditCardList from "../../components/credit-card-list/credit-card-list";
 
 const CreditCard: React.FC = () => {
   return (
     <div className="container">
-      <table className="table table-bordered">
-        <tbody>
-          <tr>
-            <td className="bg-light">Name</td>
-            <td className="bg-light">Card Number</td>
-            <td className="bg-light">Balance</td>
-            <td className="bg-light">Limit</td>
-          </tr>
-          <tr>
-            <td>Alice</td>
-            <td>1111 2222 3333 4444</td>
-            <td>£-1045</td>
-            <td>£2000</td>
-          </tr>
-          <tr>
-            <td>Bob</td>
-            <td>4444 3333 2222 1111</td>
-            <td>£10.24</td>
-            <td>£2000</td>
-          </tr>
-        </tbody>
-      </table>
+      <h1>Credit Card System</h1>
+      <AddCreditCard />
+      <br></br>
+      <CreditCardList />
     </div>
   );
 };
