2ede6cde2c5c6b3edcb8fc645b60a680db435dc4
Jerry Thimothy J <jerrythimothy@gmail.com>
2019-10-20 00:13:50 +0530

    Add form and table titles #14

--- src/components/add-credit-card/add-credit-card.tsx
+++ src/components/add-credit-card/add-credit-card.tsx
@@ -10,55 +10,60 @@ type Props = {
 
 const AddCreditCard: React.FC<Props> = props => {
   return (
-    <form>
-      <div className="form-group w-25">
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          className="form-control"
-          id="name"
-          onChange={event => {
-            props.stageInputData("name", event.target.value);
-          }}
-          value={props.stagedInputData.name || ""}
-        />
-      </div>
-      <div className="form-group w-25">
-        <label htmlFor="number">Card Number</label>
-        <input
-          type="text"
-          className="form-control"
-          id="number"
-          onChange={event => {
-            props.stageInputData("number", event.target.value);
-          }}
-          value={props.stagedInputData.number || ""}
-        />
-      </div>
-      <div className="form-group w-25">
-        <label htmlFor="limit">Limit</label>
-        <input
-          type="text"
-          className="form-control"
-          id="limit"
-          onChange={event => {
-            props.stageInputData("limit", event.target.value);
-          }}
-          value={props.stagedInputData.limit || ""}
-        />
-      </div>
-      {mapIndexed((error: any, idx) => <div className="text-danger" key={idx}>{error}</div>)(
-        props.errors
-      )}
-      <br />
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={() => props.addCreditCard()}
-      >
-        Add
-      </button>
-    </form>
+    <>
+      <h4>Add</h4>
+      <form>
+        <div className="form-group w-25">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            onChange={event => {
+              props.stageInputData("name", event.target.value);
+            }}
+            value={props.stagedInputData.name || ""}
+          />
+        </div>
+        <div className="form-group w-25">
+          <label htmlFor="number">Card Number</label>
+          <input
+            type="text"
+            className="form-control"
+            id="number"
+            onChange={event => {
+              props.stageInputData("number", event.target.value);
+            }}
+            value={props.stagedInputData.number || ""}
+          />
+        </div>
+        <div className="form-group w-25">
+          <label htmlFor="limit">Limit</label>
+          <input
+            type="text"
+            className="form-control"
+            id="limit"
+            onChange={event => {
+              props.stageInputData("limit", event.target.value);
+            }}
+            value={props.stagedInputData.limit || ""}
+          />
+        </div>
+        {mapIndexed((error: any, idx) => (
+          <div className="text-danger" key={idx}>
+            {error}
+          </div>
+        ))(props.errors)}
+        <br />
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => props.addCreditCard()}
+        >
+          Add
+        </button>
+      </form>
+    </>
   );
 };
 
